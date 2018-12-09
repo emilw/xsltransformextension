07dd4e99f38a5b8af58a3af0f35ba206ec81fca0
emilw <emil@fornby.com>
2018-12-09 14:36:00 +0100

    Added suport to pick XSL

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {xsltProcess, xmlParse} from 'xslt-processor';
+import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
+//import {xsltProcess, xmlParse} from 'xslt-processor';
+const xslLib = require('xslt-processor');
+const {xsltProcess, xmlParse} = xslLib;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,10 +15,13 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "change-file-encoding-for-many-files" is now active!');
 
+    let preselectedDisposable = vscode.commands.registerCommand('extension.applyPreselectedXSL', (lastFile: any, files: any[]) => {
+        vscode.window.showErrorMessage("There were no pre mapped XSL, please update pre map configuration", );
+    })
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', (lastFile: any, files: any[]) => {
+    let disposable = vscode.commands.registerCommand('extension.selectXSLToApply', (selectedFile: any, files: any[]) => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
@@ -28,23 +34,51 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.workspace.openTextDocument(files[0]).then((document) => {
             console.log(document.getText());
         });*/
-        vscode.workspace.openTextDocument(files[0]).then((xmlFile) => {
-            const xmlData = xmlFile.getText();
-            console.log(xmlData);
-            const xml = xmlParse(xmlData);
-            vscode.workspace.openTextDocument('/Users/Emil/Repos/XmltestFolder/Transform.xsl').then((xslFile) => {
-                const xslData = xslFile.getText();
-                console.log(xslData);
-                const xslt = xmlParse(xslData);
-                const transformedXml = xsltProcess(xml, xslt);
-                console.log(transformedXml);
-            });
+
+        let xslFilePath = '';
+
+        vscode.window.showOpenDialog({
+            canSelectFiles: true,
+            openLabel: 'Pick the XSL file to apply'
+        }).then((xslFile) => {
+            if(xslFile && xslFile.length > 0){
+                console.log(xslFile);
+                xslFilePath = xslFile[0].path;
+
+                vscode.workspace.openTextDocument(selectedFile).then((xmlFile) => {
+                    const xmlData = xmlFile.getText();
+                    console.log(xmlData);
+                    const xml = xmlParse(xmlData);
+                    
+                    //'/Users/Emil/Repos/XmltestFolder/Transform.xsl'
+                    vscode.workspace.openTextDocument(xslFilePath).then((xslFile) => {
+                        const xslData = xslFile.getText();
+                        console.log(xslData);
+                        const xslt = xmlParse(xslData);
+                        const transformedXml = xsltProcess(xml, xslt);
+                        console.log(transformedXml);
+                        vscode.workspace.openTextDocument({
+                            content: transformedXml,
+                            language: 'xml'
+                        }).then((textDoc) => {
+                            vscode.window.showTextDocument(textDoc).then((textEditor) => {
+                                vscode.commands.executeCommand('editor.action.formatDocument').then((x) => {
+                                    
+                                });
+                            });
+                        });
+                    });
+                });
+            }
         });
 
     });
 
+
+
     //context.subscriptions.push(vscode.window.)
     context.subscriptions.push(disposable);
+    context.subscriptions.push(preselectedDisposable);
 }
 
 // this method is called when your extension is deactivated
